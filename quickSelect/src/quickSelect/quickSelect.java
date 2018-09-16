68ff4c62051d08c53323514ac66e7277c24118d9
jester9108 <kimavi@bc.edu>
2018-09-15 23:06:30 -0400

    modified with finding median of medians approach

--- quickSelect/src/quickSelect/quickSelect.java
+++ quickSelect/src/quickSelect/quickSelect.java
@@ -1,5 +1,7 @@
 package quickSelect;
 
+import java.util.*;
+
 //Finding the kth biggest element from an unsorted list.
 //Average complexity is O(n)
 
@@ -15,18 +17,19 @@ public class quickSelect {
 		}
 		
 		for(;;) {
-			int pivot = left;
-			int pivotVal = arr[pivot];
+			int pivotVal = medianOfMedians(arr, left, right);
 			
-			swap(arr, pivot, right);
 			int current = left;
-			
 			for(int i = left; i < right; i++) {
 				//greater than and not equal to, in order to restore pivot at the end
 				
 				if(arr[i] > pivotVal) {
 					//no need to sort
 					swap(arr, current++, i);
+				}
+				//send the pivot value to the end
+				else if(arr[i] == pivotVal) {
+					swap(arr, i--, right);
 				}
 			}
 			swap(arr, current, right);
@@ -49,6 +52,66 @@ public class quickSelect {
 		}
 	}
 	
+
+	private static int medianOfMedians(int[] arr, int left, int right) {
+		List<Integer> medArr = new ArrayList<>();
+		
+		int count = left;
+		int numOfSubarraies = (int)Math.ceil((right - left)/5.0);
+		
+		for(int i = 0; i < numOfSubarraies; i++) {
+			List<Integer> sub = new ArrayList<>();
+			for(int j = 0; j < 5; j++) {
+				if(count == right) {
+					break;
+				}
+				sub.add(arr[count++]);
+			}
+			medArr.add(findingMedianFromFive(sub));
+		}
+		int median = medArr.get(0);
+		if(medArr.size() > 2) {
+			median = recursiveMedianSearch(medArr);
+		}
+		
+		return median;
+	}
+	
+	private static int recursiveMedianSearch(List<Integer> li) {
+		int len = li.size();
+		
+		if (len <= 2)
+			return li.get(0);
+		
+		else {
+			List<Integer> medArr = new ArrayList<>();
+			
+			int count = 0;
+			int numOfSubarraies = len/5;
+			
+			for(int i = 0; i < numOfSubarraies; i++) {
+				List<Integer> sub = new ArrayList<>();
+				for(int j = 0; j < 5; j++) {
+					if(count == len) {
+						break;
+					}
+					sub.add(li.get(count++));
+				}
+				medArr.add(findingMedianFromFive(sub));
+			}
+			return recursiveMedianSearch(medArr); 
+		}
+	}
+	
+	private static int findingMedianFromFive(List<Integer> arr) {
+		if(arr.size() < 3) {
+			return arr.get(0);
+		}
+		arr.sort((Integer i1, Integer i2)->{return i1 < i2? -1: i1 > i2? 1 : 0;});
+		return arr.get(2);
+	}
+	
+	
 	private static void swap(int[] arr, int ind1, int ind2) {
 		int temp = arr[ind1];
 		arr[ind1] = arr[ind2];
@@ -57,7 +120,13 @@ public class quickSelect {
 	}
 	
 	public static void main(String[] args) {
-		int[] a = {2,3,4,5,7,8,9,10,11,12};
-		System.out.println(select(a, 5));
+		int[] a = {2,4233,2344,541,712,813,913,13240,32411,12342};
+		
+		for(int i = 0; )
+		System.out.println(select(a, 1));
+		Arrays.sort(a);
+		for(int x : a) {
+			System.out.print(x + ", ");
+		}
 	}
 }
